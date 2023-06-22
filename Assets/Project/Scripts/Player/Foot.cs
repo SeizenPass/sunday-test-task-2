10f3c8ec7f6b46ef2bb71cf5fa6b459a17569856
Amiran Kurman <amiran.k@aloteq.com>
2023-06-22 13:59:37 +0600

    footprint

---
+++ Assets/Project/Scripts/Player/Foot.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Project.Scripts.Utils;
+using UnityEngine;
+using UnityEngine.Events;
+
+namespace Project.Scripts.Player
+{
+    public class Foot : MonoBehaviour
+    {
+        [SerializeField] private LayerMask groundLayer;
+
+        public UnityEvent<Vector3> onContact;
+
+        private void OnTriggerEnter(Collider other)
+        {
+            if (LayerMaskUtils.CompareLayerMasks(groundLayer, other.gameObject.layer))
+            {
+                onContact.Invoke(other.ClosestPointOnBounds(transform.position));
+            }
+        }
+    }
+}
---
+++ Assets/Project/Scripts/Player/FootprintGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using Project.Scripts.Pool;
+using UnityEngine;
+
+namespace Project.Scripts.Player
+{
+    public class FootprintGenerator : MonoBehaviour
+    {
+        [SerializeField] private ObjectPool footPrintPool;
+        [SerializeField] private Transform player;
+        [SerializeField] private float lifetime = 6f;
+        
+
+        public void LeaveFootprint(Vector3 pos)
+        {
+            var ftp = footPrintPool.GetPooledObject();
+
+            ftp.transform.position = pos;
+            ftp.transform.rotation = player.rotation;
+            ftp.SetActive(true);
+            StartCoroutine(FootprintLifetime(ftp));
+        }
+
+        private IEnumerator FootprintLifetime(GameObject ftp)
+        {
+            yield return new WaitForSeconds(lifetime);
+            ftp.SetActive(false);
+        }
+    }
+}
