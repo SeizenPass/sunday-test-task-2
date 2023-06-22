b3a5e5de241a5cf07501b47e65cac892c71063d0
Amiran Kurman <amiran.k@aloteq.com>
2023-06-22 17:13:23 +0600

    fps count

---
+++ Assets/Project/Scripts/FPSCounter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using TMPro;
+using UnityEngine;
+
+namespace Project.Scripts
+{
+    public class FPSCounter : MonoBehaviour
+    {
+        [SerializeField] private TextMeshProUGUI fpsText;
+
+        private void Update()
+        {
+            if (fpsText) fpsText.text = $"FPS: {(int)(1f / Time.unscaledDeltaTime)}";
+        }
+    }
+}
---
+++ Assets/Project/Scripts/FPSUnlocker.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+
+namespace Project.Scripts
+{
+    public class FPSUnlocker : MonoBehaviour
+    {
+        public void SetTargetFramerate(int fps)
+        {
+            if (fps <= 0) return;
+            Application.targetFrameRate = fps;
+        }
+    }
+}
